191b3a103da889bb8e9dcd09104d018e37f924b1
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-08-02 13:08:35 -0400

    added 'pivot' regexp'

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ function respond() {
 	  botRegexStartup = /^startup/i;
 	  botRegexValue = /^value/i;
 	  botRegexNetwork = /^network/i;
+	  botRegexPivot = /^pivot/i;
 
   if(request.text && botRegexIterate.test(request.text)) {
     this.res.writeHead(200);
@@ -52,6 +53,12 @@ function respond() {
   if(request.text && botRegexNetwork.test(request.text)) {
 	this.res.writeHead(200);
     postMessageNetwork();
+    this.res.end();
+  }
+  
+  if(request.text && botRegexPivot.test(request.text)) {
+	this.res.writeHead(200);
+    postMessagePivot();
     this.res.end();
   }
   
@@ -315,6 +322,41 @@ function postMessageNetwork() {
   var botResponse, options, body, botReq;
   
   botResponse =' NETWORKING';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessagePivot() {
+  var botResponse, options, body, botReq;
+  
+  botResponse =' PIVOT';
 
   options = {
     hostname: 'api.groupme.com',
