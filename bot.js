255969588632bc79a1aa90df2cb6a0858f9ec857
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-08-02 12:53:24 -0400

    added 'platform' regexp

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ function respond() {
       botRegexIterate = /^iterat/i;
 	  botRegexDisrupt = /^disrupt/i;
 	  botRegexYin = /^yin/i;
+	  botRegexPlatform = /^platform/i;
 	  botRegexAutomate = /^automat/i;
 
   if(request.text && botRegexIterate.test(request.text)) {
@@ -24,6 +25,12 @@ function respond() {
   if(request.text && botRegexYin.test(request.text)) {
 	this.res.writeHead(200);
     postMessageYin();
+    this.res.end();
+  } 
+  
+  if(request.text && botRegexPlatform.test(request.text)) {
+	this.res.writeHead(200);
+    postMessagePlatform();
     this.res.end();
   } 
   
@@ -147,6 +154,41 @@ function postMessageAutomate() {
   var botResponse, options, body, botReq;
   
   botResponse =' AUTOMATION IS 2 SPOOKY';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessagePlatform() {
+  var botResponse, options, body, botReq;
+  
+  botResponse =' PLATFORM';
 
   options = {
     hostname: 'api.groupme.com',
