918d9a3461540a18184bfcf567aef4de658e2fbe
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-08-02 12:05:29 -0400

    testing regexp pt 3?

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegexGuy = /^guy/i;
+      botRegexGuy = /?=guy/i;
 	  botRegexGal = /^\/gal$/;
 	  botRegexNeutral = /^\/neutral$/;
 	  botRegexActivity = /^\/activity$/;
