51a42a93f7b7ebe6d0a60f52a1889fbf1d527883
Guilaine Jean-Gilles <Guilaine Jean-Gilles>
2017-08-02 12:31:34 -0400

    hot fix spam

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegexIterate = /^iterat/i;
+      //botRegexIterate = /^iterat/i;
 	  botRegexGal = /^gal/i;
 	  botRegexNeutral = /^neutral/i;
 	  botRegexActivity = /^activity/i;
